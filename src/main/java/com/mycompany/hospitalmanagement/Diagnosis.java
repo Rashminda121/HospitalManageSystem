2337a221ec6bdb3c0a25f62f85669e852cbd4bcf
rashminda <rashmindaeducation@gmail.com>
2024-01-06 19:10:53 +0530

    commit diagnosis

--- src/main/java/com/mycompany/hospitalmanagement/Diagnosis.java
+++ src/main/java/com/mycompany/hospitalmanagement/Diagnosis.java
@@ -9,10 +9,7 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Rashminda
- */
+
 public class Diagnosis extends javax.swing.JFrame {
 
     /**
