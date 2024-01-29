5f2ecd8f41b841c31d20819f57fc41f040a2deb8
Rashminda <rashminda121@gmail.com>
2024-01-29 21:06:45 +0530

    Update Doctors.java

--- src/main/java/com/mycompany/hospitalmanagement/Doctors.java
+++ src/main/java/com/mycompany/hospitalmanagement/Doctors.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/
  */
 package com.mycompany.hospitalmanagement;
 
